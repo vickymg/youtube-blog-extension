8d17c626f2b4b0d91eb0080a624fe365bcc2b3ca
Vicky Gray <vicky@buzzmyvideos.com>
2016-08-04 15:28:07 +0100

    Added listener to open links in new tab

--- getfeed.js
+++ getfeed.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
   $("#feed").PaRSS("http://feeds.feedburner.com/youtube/PKJx",
   5,
   "M jS g:i a",
-  true)
+  true);
+
+  window.addEventListener('click', function(e) {
+    if (e.target.href !== undefined) {
+      chrome.tabs.create({ url: e.target.href })
+    }
+  });
 });
